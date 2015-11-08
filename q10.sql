69ea99b6d23a23941645f53d75f618b0f1a78713
Huang Wilson <wilsonwei.huang@mail.utoronto.ca>
2015-11-08 18:04:12 -0500

    q9 q10

--- q10.sql
+++ q10.sql
@@ -1,1 +1,26 @@
 SET search_path TO artistdb;
+
+CREATE VIEW Thriller_Songs AS
+SELECT Song.song_id
+FROM Song, Album, BelongsToAlbum
+WHERE Song.song_id = BelongsToAlbum.Songid AND Album.album_id = BelongsToAlbum.album_id AND Album.title = 'Thriller';
+
+DELETE FROM Collaboration
+WHERE song_id IN Thriller_Songs;
+
+DELETE FROM Song 
+WHERE Song.song_id IN Thriller_Songs;
+
+DELETE FROM ProducedBy
+WHERE ProducedBy.album_id IN (SELECT Album.album_id FROM Album WHERE Album.title = 'Thriller');
+
+DELETE FROM BelongsToAlbum 
+WHERE BelongsToAlbum.song_id IN Thriller_Songs OR BelongsToAlbum.album_id = (SELECT Album.album_id FROM Album WHERE Album.title = 'Thriller');
+
+DELETE FROM Album
+Where Album.title = 'Thriller'
+
+
+
+---update Role?
+
--- q9.sql
+++ q9.sql
@@ -1,1 +1,11 @@
+---NEEDS FIXING BASED ON PIAZZA RESPONSES: THE MAIN CONCERN IS WHETHER MICK JAGGER LEFT ROLLING STONES YEARS AGO, IN WHICH CASE WE DO NOT NEED TO UPDATE HIS END YEAR IN THE WAS_IN_BAND RELATION
 SET search_path TO artistdb;
+--- updates the WasInBand relation so that Adam Levine was w/ M5 up until 2014, and MJ was w/ R.S. til 2014. Need to check of MJ was in Rolling Stones up until 2015?
+
+UPDATE WasInBand
+SET end_year = '2014' 
+---we are given the assumption that adam levine and mick jagger only appear in this relation once, since they have "only ever been in one bad, uninterrupted time frame"
+WHERE artist_id IN (SELECT artist_id FROM Artist WHERE name = 'Adam Levine' OR name = 'Mick Jagger');
+---Insert a new entry to WasInBand with Mick Jagger belonging to Maroon 5, starting from 
+INSERT INTO WasInBand
+VALUES ((SELECT artist_id FROM Artist WHERE name = 'Mick Jagger'),(SELECT artist_id FROM Artist WHERE name = 'Maroon 5'), 2014, 2015);
