659b23b9081108cf25c590e6010ed910a18642de
Shin Wooyoung <c5shinwo@cdf.toronto.edu>
2015-11-08 18:29:03 -0500

    Questions 1 and 2 completed

--- q1.sql
+++ q1.sql
@@ -3,7 +3,6 @@ CREATE VIEW Steppenwolf_Album_Years AS(
 SELECT year FROM Artist, Album where Artist.name = 'Steppenwolf' and Artist.artist_id = Album.artist_id);
 
 SELECT DISTINCT name, nationality
-FROM Artist
-WHERE Extract( year FROM birthdate ) =
-(SELECT min(year) FROM Steppenwolf_Album_Years) 
+FROM Artist, Role
+WHERE Artist.artist_id = Role.artist_id and Role.role = 'Musician' and Extract( year FROM birthdate ) =( SELECT min(year) FROM Steppenwolf_Album_Years ) 
 ORDER BY name ASC;
