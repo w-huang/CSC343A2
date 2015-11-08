727690d6f9a4d5cf21a71b714712a196763a0913
Huang Wilson <wilsonwei.huang@mail.utoronto.ca>
2015-11-08 16:44:57 -0500

    q5, q7

--- q5.sql
+++ q5.sql
@@ -1,1 +1,11 @@
 SET search_path TO artistdb;
+
+CREATE VIEW Not_Being_Yourself AS
+SELECT DISTINCT Album.album_id
+FROM Album, BelongsToAlbum, Song
+WHERE Album.album_id = BelongsToAlbum.album_id AND BelongsToAlbum.song_id = Song.song_id AND Song.songwriter_id != Album.artist_id;
+
+SELECT Artist.name, Album.name
+FROM Artist, Album
+WHERE Album.album_id NOT IN Not_Being_Yourself AND Artist.artist_id = Album.artist_id
+ORDER BY Artist.name ASC; 
--- q7.sql
+++ q7.sql
@@ -1,1 +1,13 @@
 SET search_path TO artistdb;
+
+CREATE VIEW Covered_Songs AS
+SELECT song_id
+FROM BelongsToAlbum
+GROUP BY song_id
+HAVING count(song_id) > 1;
+
+SELECT Song.title, Album.year, Artist.name
+FROM Song, Album, Artist, BelongsToAlbum
+WHERE Song.song_id = BelongsToAlbum.song_id AND Album.album_id = BelongsToAlbum.album_id AND Song.song_id IN Covered_Songs
+ORDER BY Song.title, Album.year, Artist.name;
+
